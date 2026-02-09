48be21744d227edc943fc9f7d771940dcab8612f
vaish224 <vaishftp@gmail.com>
2026-02-09 13:56:43 +0530

    first commit

---
+++ python.py
@@ -0,0 +1,66 @@
+x=int(input("enter the first statement"))
+y=int(input("enter the second statement "))
+if x > y :
+   print("x greatest ")
+
+else:
+    print(" y gretest")  
+
+a = int(input("enter the c marks"))
+b = int(input("enter the c marks"))
+c = int(input("enter the c marks"))
+# = int(input("enter the c marks"))
+x=a+b+c
+grad=x/3
+if grad>=90:
+    print("divisin i A")
+elif grad>=75:
+    print("grad is b")
+elif grad>=50:
+    print("grad is c")
+
+
+else :
+    print("fail")
+a = int(input("enter the year"))
+leap =a/4 
+if (leap==0):
+    print("this is leap year")
+else:
+    print("this is not leap year")    
+
+a = int(input("enter the c marks"))
+# b= int(input("enter the c marks"))
+# c = int(input("enter the c marks"))
+if a <10:
+    print("its single didgit number")
+elif a <100:
+    print("this is double digit")    
+else  :
+ print(" whwter is more than two didgit")
+a = int(input("enter the c marks"))
+#b= int(input("enter the c marks"))
+#c = int(input("enter the c marks"))
+if a <20:
+    print(" wether is cold")
+elif a <35:
+    print("weather is warm")    
+else  :
+ print(" weather is hot")
+
+a = int(input("enter the c marks"))
+#b= int(input("enter the c marks"))
+#c = int(input("enter the c marks"))
+if a >0:
+    print("number is positive")
+    if a%2==0:
+       print(a,"this is even number")
+    else:
+       print(a,"this odd number")    
+else  :
+    print("this is negative number")
+
+
+
+
+
